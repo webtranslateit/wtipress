176768034aa79bd6b1faf9fef828e7359b6ef333
Édouard Brière <edouard.briere@gmail.com>
2011-09-15 14:24:31 +0200

    Fix header information already sent warnings, close #15

--- lib/wtipress.class.php
+++ lib/wtipress.class.php
@@ -8,8 +8,7 @@ class WtiPress {
   function __construct() {
     add_action('admin_menu', array($this, 'administration_menu'));
     $this->settings = new Setting();
-    $this->admin_warning();
-    $this->version_warning();
+    $this->warnings();
     if(isset($this->settings->api_key)) {
       // fetch project settings
       $this->network = new Network($this->settings->api_key);
@@ -66,23 +65,7 @@ class WtiPress {
     add_submenu_page(basename(WTIPRESS_PLUGIN_PATH).'/menu/setup.php', "Setup", "Setup", 'manage_options', basename(WTIPRESS_PLUGIN_PATH).'/menu/setup.php');
     add_submenu_page(basename(WTIPRESS_PLUGIN_PATH).'/menu/setup.php', "Setup", "Translations", 'manage_options', basename(WTIPRESS_PLUGIN_PATH).'/menu/translations.php');
   }
-  
-  function admin_warning() {
-    if (!isset($this->settings->api_key) && !isset($_POST['wti_api_key'])) {
-      echo "<div class='updated fade'><p><strong>WTIpress is almost ready.</strong> You must <a href='admin.php?page=wtipress/menu/setup.php'>enter your WebTranslateIt API key</a> for it to work.</p></div>";
-    }
-  }
-  
-  function version_warning() {
-    global $wp_version;
-    if (version_compare($wp_version, WTIPRESS_MIN_WORDPRESS_VERSION, '<')) {
-      echo "<div class='updated fade'><p><strong>WTIpress ".WTIPRESS_VERSION." requires WordPress ".WTIPRESS_MIN_WORDPRESS_VERSION." or higher.</strong> Please <a href='http://codex.wordpress.org/Upgrading_WordPress'>upgrade WordPress</a> to a current version.</p></div>";
-    }
-    if(version_compare(phpversion(), '5', '<')) {
-      echo "<div class='updated fade'><p><strong>WTIpress requires PHP 5 or higher.</strong> Please upgrade PHP to a current version.</p></div>";
-    }    
-  }
-  
+      
   function process_forms() {
     if (isset($_POST['wti_api_key'])) {
       $this->settings->api_key = $_POST['wti_api_key'];
@@ -95,6 +78,34 @@ class WtiPress {
       elseif ($_POST['action'] == 'pull') {
         $this->settings->pull_post($_POST['wti_post_id']);
       }
+    }
+  }
+  
+  function warnings() {
+    global $wp_version;
+
+    if (version_compare($wp_version, WTIPRESS_MIN_WORDPRESS_VERSION, '<')) {
+      add_action('admin_notices', 'wtipress_wordpress_version_warning');
+    }
+
+    if(version_compare(phpversion(), '5', '<')) {
+      add_action('admin_notices', 'wtipress_php_version_warning');
+    }
+
+    if (!isset($this->settings->api_key) && !isset($_POST['wti_api_key'])) {
+      add_action('admin_notices', 'wtipress_almost_ready');
+    }
+
+    function wtipress_wordpress_version_warning() {
+      echo "<div class='updated fade'><p><strong>WTIpress ".WTIPRESS_VERSION." requires WordPress ".WTIPRESS_MIN_WORDPRESS_VERSION." or higher.</strong> Please <a href='http://codex.wordpress.org/Upgrading_WordPress'>upgrade WordPress</a> to a current version.</p></div>";
+    }
+
+    function wtipress_php_version_warning() {
+      echo "<div class='updated fade'><p><strong>WTIpress requires PHP 5 or higher.</strong> Please upgrade PHP to a current version.</p></div>";
+    }
+
+    function wtipress_almost_ready() {
+      echo "<div class='updated fade'><p><strong>WTIpress is almost ready.</strong> You must <a href='admin.php?page=wtipress/menu/setup.php'>enter your WebTranslateIt API key</a> for it to work.</p></div>";
     }
   }
 }
