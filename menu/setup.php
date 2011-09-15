f20052cd5e286213d24a08f1af1e343f7b35661f
Édouard Brière <edouard.briere@gmail.com>
2011-09-15 17:15:00 +0200

    Present languages in a table

--- menu/setup.php
+++ menu/setup.php
@@ -11,40 +11,55 @@
   <div id="icon-options-general" class="icon32"><br /></div>
   <h2>Setup WTIpress</h2>
     
-  <h3>WebTranslateIt.com API key</h3>
   <form id="wti_api_key" method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
-    <?php wp_nonce_field('wti_api_key') ?>            
-    <p>
-      <input type="text" name="wti_api_key" value="<?php echo $wtipress->settings->api_key ?>" />
-      You will find your API key in your project settings<br /><br />
+    <table class="form-table">
+      <tbody>
+        <tr valign="top">
+          <th scope="row">
+            <label for="wti_api_key">WebTranslateIt.com API key</label>
+          </th>
+          <td>
+            <input type="text" name="wti_api_key" id="wti_api_key" value="<?php echo $wtipress->settings->api_key ?>" />
+            <span class="description">You will find your API key in your project settings.</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    
+    <p class="submit">
       <input class="button-primary" name="save" value="Save and Refresh" type="submit" />
     </p>
   </form>
   
-  <?php
-  if(isset($wtipress->settings->api_key)) {
-  ?>
+  <?php if(isset($wtipress->settings->api_key)) { ?>
   
   <h3>Languages at play</h3>
 
   <p>
-    Tip: Add languages on your <?php echo '<a href="https://webtranslateit.com/projects/'.$wtipress->settings->project_id.'">project on WebTranslateIt.com</a>.'; ?>
+    Tip: Add languages on your <?php echo '<a href="https://webtranslateit.com/projects/'.$wtipress->settings->project_id.'">project on WebTranslateIt.com</a> and refresh this form for them to appear.'; ?>
     We detected the following:
   </p>
   
-  <h4>Source:</h4>
-  <?php
-  $source_language = Language::get_source_language();
-  echo "<ul><li>" . $source_language[0]->name . "</li></ul>";
-  ?>
-  
-  <h4>Target:</h4>
-  <?php
-  echo "<ul>";
-  foreach(Language::get_all() as $language) {
-    echo "<li>".$language->name . "</li>";
-  }
-  echo "</ul>";
-}
-?>
+  <table class="wp-list-table widefat fixed languages" cellspacing="0">
+  	<thead>
+  	<tr>
+  		<th scope='col' id='name' class='manage-column column-name'><span>Language Name</span></th>
+  		<th scope='col' id='type' class='manage-column column-type'>Type</th>
+    </tr>
+    </thead>
+  	<tbody id="the-list">
+  	  <?php $source_language = Language::get_source_language(); ?>
+  	  <tr id='post-8' class='alternate author-self status-publish format-default iedit' valign="top">
+  	    <td class="language-name page-title column-title"><strong><?php echo $source_language[0]->name; ?></a></strong></td>
+        <td class="date column-type">Source</td>
+      </tr>
+      <?php foreach(Language::get_all() as $language) { ?>
+        <tr id='post-8' class='alternate author-self status-publish format-default iedit' valign="top">
+    	    <td class="language-name page-title column-title"><strong><?php echo $language->name; ?></a></strong></td>
+          <td class="date column-type">Target</td>
+        </tr>
+    	<?php }  ?>
+    </tbody>
+  </table>
+  <?php } ?>
 </div>
