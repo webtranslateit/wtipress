4e9079953aedb0c43a02450482673cbc4e8d76dd
edouardb <edouardb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-20 05:48:41 +0000

    List all posts for now
    git-svn-id: http://plugins.svn.wordpress.org/wtipress/trunk@453208 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menu/translations.php
+++ menu/translations.php
@@ -1,1 +1,11 @@
-Hello
+<?php
+
+$posts = get_posts(-1);
+
+foreach($posts as &$post) {
+  echo '<li><a href="'.$_SERVER['REQUEST_URI'].'?action=push&id='.$post->ID.'">';
+  echo $post->post_title;
+  echo "</a></li>";
+}
+
+?>
